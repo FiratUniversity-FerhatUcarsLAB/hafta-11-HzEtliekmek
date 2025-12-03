35e17c79563515aad0d96551613ccf565e20b3ff
Muhammed Said Bestil <73086933+HzEtliekmek@users.noreply.github.com>
2025-12-03 23:54:28 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,25 @@
 public class Exercise4_1 {
 
+    public static void main(String[] args) {
+        String day = "Monday";
+        String month = "July";
+        int date = 22;
+        int year = 2019;
+
+        System.out.println("Amerikan Formatı:");
+        printAmerican(day, date, month, year);
+
+        System.out.println("Avrupa Formatı:");
+        printEuropean(day, date, month, year);
+    }
+
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+        // Format: Monday, July 22, 2019
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
-    }
-
-    public static void main(String[] args) {
-        // TODO: test methods
+        // Format: 22 July 2019, Monday
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 }
