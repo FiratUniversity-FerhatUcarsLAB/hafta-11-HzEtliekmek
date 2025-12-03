66865ac7a1025c4c587ea9fe2d04e1407e8d810d
Muhammed Said Bestil <73086933+HzEtliekmek@users.noreply.github.com>
2025-12-03 23:55:08 +0300

    Update Exercise4_2.java

--- Exercise4_2.java
+++ Exercise4_2.java
@@ -1,23 +1,28 @@
 public class Exercise4_2 {
 
-  // Her satirin sonuna calisma sirasi yorumlarini ekleyin.
-
     public static void main(String[] args) {
-        zippo("rattle", 13);
+        zippo("rattle", 13);               // 1
     }
 
     public static void baffle(String blimp) {
-        System.out.println(blimp);
-        zippo("ping", -5);
+        System.out.println(blimp);         // 4
+        zippo("ping", -5);                 // 5
     }
 
     public static void zippo(String quince, int flag) {
-        if (flag < 0) {
-            System.out.println(quince + " zoop");
+        if (flag < 0) {                    // 2, 6
+            System.out.println(quince + " zoop"); // 7
         } else {
-            System.out.println("ik");
-            baffle(quince);
-            System.out.println("boo-wa-ha-ha");
+            System.out.println("ik");      // 3
+            baffle(quince);                // 4 (Buradan baffle çağrılır)
+            System.out.println("boo-wa-ha-ha"); // 8
         }
     }
 }
+/*
+ * ÇIKTI:
+ * ik
+ * rattle
+ * ping zoop
+ * boo-wa-ha-ha
+ */
